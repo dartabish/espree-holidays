cd76bcdd1e7152b34f6f6dbcdde28123910593ce
dartabish <dartabish01@gmail.com>
2024-03-06 15:33:21 +0530

    refactor

--- subpages/7-skiing & snowboarding/script.js
+++ subpages/7-skiing & snowboarding/script.js
@@ -66,37 +66,39 @@ const courses = [
         day1: {
           id: 1,
           title: 'Day 1: Arrival and Acclimatization',
-          description: `Activities: Arrive in Gulmarg and get accustomed to the altitude. Leisurely explore the surroundings. Evening briefing about the course, skiing equipment, and safety protocols.`,
+          description: `Activities: Arrive in Gulmarg and get accustomed to the altitude. Leisurely explore the surroundings. Evening briefing about the course, snowboarding equipment, and safety protocols.`,
         },
         day2: {
           id: 2,
-          title: 'Day 2: Skiing Basics and Practice',
-          description: `Morning Session: Learn basic skiing techniques, including the correct posture, balance, and the plow stop technique. Afternoon Session: Practice these basics on gentle slopes under instructor supervision.`,
+          title: 'Day 2: Grabbing your snowboards',
+          description: `You will be provided with top-of-the-line snowboarding gear to ensure a safe and comfortable experience. This includes a snowboard, boots, bindings, gloves, helmet, and goggles. Our team will take the time to guide you on the proper use of each piece of equipment, so you can confidently hit the slopes with peace of mind.`,
         },
         day3: {
           id: 3,
-          title: 'Day 3: Advancing Skills',
-          description: `Morning Session: Recap of Day 2 lessons, followed by instruction on turning techniques and speed control. Afternoon Session: Practice these new skills on slightly steeper slopes.`,
+          title: 'Day 3: Learning Snowbaording Basics',
+          description: `During the third day of your exhilarating journey, a seasoned snowboarding coach will be at your service to navigate you through the essential techniques of snowboarding. This expert, committed to your learning and safety, will ensure you grasp the foundational aspects of the sport while having a delightful experience on the slopes. Leveraging their profound knowledge and tailored guidance, you can be confident that by the time you depart from Gulmarg, you'll possess a comprehensive understanding of snowboarding, along with the assurance to master the slopes effortlessly.`,
         },
         day4: {
           id: 4,
           title: 'Day 4: Skill Refinement',
-          description: `Morning Session: Introduction to advanced techniques like parallel turns and carving. Afternoon Session: Practice on varied terrain to refine these skills and build confidence.`,
+          description: `On the fourth day of your adventure, the focus shifts towards refining your snowboarding abilities. Under the guidance of our adept instructor, you'll delve into the critical techniques vital for ensuring a secure and pleasurable time on the slopes. You'll cover everything from maintaining balance on your board to executing smooth turns and coming to a stop efficiently.`,
         },
         day5: {
           id: 5,
-          title: 'Day 5: Exploring the Slopes',
-          description: `Morning Session: Apply the learned skills on different slopes of Gulmarg under guidance. Afternoon Session: Feedback session with instructors on progress and areas for improvement.`,
+          title: 'Day 5: Testing Your Skills',
+          description: `On the fifth day of your journey, you're poised to challenge your freshly acquired skills and techniques. Empowered with the confidence and insight nurtured through the preceding days of training, you'll be presented with the chance to showcase your snowboarding prowess in a definitive test.`,
         },
         day6: {
           id: 6,
-          title: 'Day 6: Skiing from Phase 1',
-          description: `With the confidence and control you have gained from your previous lessons, you will be able to experience the thrill of skiing from Phase 1 to the base.`,
+          title: 'Day 6: Snowboarding from Phase 1',
+          description: `
+          On the sixth day of your expedition, you're ready to elevate your snowboarding abilities. You'll challenge yourself on the more demanding slopes, venturing beyond previous boundaries and experiencing snowboarding's exhilaration in unparalleled ways. Guided and supported by our seasoned instructor, you'll progress from beginner stages to more advanced techniques, demonstrating your enhanced skills and savoring every moment of the descent.`,
         },
         day7: {
           id: 7,
-          title: 'Day 7: Departure',
-          description: `On the seventh and final day of your adventure, you will depart from the slopes and make your way back home, filled with memories of your amazing skiing experience.`,
+          title: 'Day 7: Back To Home',
+          description: `
+          As your adventure reaches its culmination, the time has come to bid farewell to Gulmarg and return home, carrying a treasure trove of indelible memories. Your seven-day snowboarding odyssey concludes here, yet the skills and memories acquired will endure forever. We're grateful you selected us to navigate you through this remarkable journey! `,
         },
       },
     },
@@ -110,7 +112,8 @@ const courses = [
         day: {
           id: 1,
           title: 'Two Week Course Details: ',
-          description: `With our fortnight-long skiing program, you're presented with a unique chance to dive deep into the skiing universe and discover everything this incredible sport encompasses. Our detailed itinerary for a week's worth of skiing activities, paired with access passes for both Phase 1 and Phase 2 for the entire week, ensures you have sufficient time and resources to polish your skiing prowess, boost your self-assurance, and enjoy the thrill of the slopes. For those craving a more demanding adventure, our package also offers the chance to elevate your skiing capabilities and test your boundaries by taking on the slopes from the pinnacle of Mount Apharwat Peak.`,
+          description: `
+          Our fortnight package includes a comprehensive schedule of activities spanning seven days, and to enrich your experience, we provide weekly passes granting access to both Phase 1 and Phase 2 areas. Furthermore, this package offers you the chance to further refine your snowboarding skills with an exhilarating descent from the summit of Mount Apharwat Peak.`,
         },
       },
     },
